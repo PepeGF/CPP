a6632cc4688247b72628138630b490c60ce154d3
Jose Garcia de la fuente <gran.pepe@gmail.com>
2023-05-19 19:51:47 +0200

    ex04 file read

--- CPP01/ex04/main.cpp
+++ CPP01/ex04/main.cpp
@@ -1,6 +1,11 @@
+#include <iostream>
+#include <fstream>
 
 int	main(int argc, char *argv[])
 {
+	std::ifstream	infile(argv[1]);
+	std::string		buffer;
+	std::size_t		found;
 	if (argc != 4)
 	{
 		std::cout 
@@ -8,7 +13,27 @@ int	main(int argc, char *argv[])
 			<< std::endl
 			<< "Usage ./my_sed infile reemplaced_string substitute_string"
 			<< std::endl;
-		
+		return (1);
 	}
-
+{
+	//probablemente sea lo mismo que verificar !infile
+	// if (infile.good() == false)
+	// {
+	// 	std::cout << "Error: unable to open " << argv[1] << std::endl;
+	// }
+	}
+	if(!infile)
+	{
+		std::cout << "Error: " << argv[1] << " not found" << std::endl;
+		exit (2);
+	}
+	while (std::getline(infile, buffer))
+	{
+		found = buffer.find((std::string)argv[2]);
+		if (found != std::string::npos)
+		{
+			std::cout << "text founded in psition " << found << std::endl;
+		}
+		std::cout << buffer << std::endl;
+	}
 }
