c8fb536828d3ca619dc4f27df35a533125ad2c5f
PepeGF <gran.pepe@gmail.com>
2023-05-19 13:43:16 +0200

    starting ex04

---
+++ CPP01/ex04/main.cpp
@@ -0,0 +1,14 @@
+
+int	main(int argc, char *argv[])
+{
+	if (argc != 4)
+	{
+		std::cout 
+			<< "Error: wrong number of arguments"
+			<< std::endl
+			<< "Usage ./my_sed infile reemplaced_string substitute_string"
+			<< std::endl;
+		
+	}
+
+}
