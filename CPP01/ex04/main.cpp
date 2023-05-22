9e90233ac9f0ba40b3b1248d6221c5ffa44c8395
PepeGF <gran.pepe@gmail.com>
2023-05-22 22:21:01 +0200

    cpp01/ex04 completed

--- CPP01/ex04/main.cpp
+++ CPP01/ex04/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <fstream>
 
+void	input_error(int argc, char *argv[]);
+void	infile_error(char **argv);
+void	outfile_error(std::string buffer);
+
 int	main(int argc, char *argv[])
 {
 	std::ifstream	infile(argv[1]);
@@ -8,6 +12,38 @@ int	main(int argc, char *argv[])
 	std::string		aux;
 	std::size_t		found;
 	std::size_t		aux_index;
+
+	input_error(argc, argv);	
+	if(!infile)
+		infile_error(argv);
+	buffer = argv[1];
+	buffer = buffer.append(".replace");
+	std::ofstream	outfile(buffer);
+	if (!outfile)
+		outfile_error(buffer);
+	while (std::getline(infile, buffer))
+	{
+		found = 0;
+		aux_index = 0;
+		aux = "";
+		while (true)
+		{
+			found = buffer.find((std::string)argv[2], aux_index);
+			if (found == std::string::npos)
+				break;
+			aux.append(buffer, aux_index, found - aux_index);
+			aux.append(std::string(argv[3]));
+			aux_index = found + std::string(argv[2]).length();
+		}
+		aux.append(buffer, aux_index);
+		outfile << aux << std::endl;
+	}
+	infile.close();
+	outfile.close();
+}
+
+void	input_error(int argc, char *argv[])
+{
 	if (argc != 4)
 	{
 		std::cout 
@@ -15,45 +51,23 @@ int	main(int argc, char *argv[])
 			<< std::endl
 			<< "Usage ./my_sed infile reemplaced_string substitute_string"
 			<< std::endl;
-		return (1);
+		exit (1);
 	}
-{
-	//probablemente sea lo mismo que verificar !infile
-	// if (infile.good() == false)
-	// {
-	// 	std::cout << "Error: unable to open " << argv[1] << std::endl;
-	// }
+	if (((std::string)argv[2]).length() == 0 || ((std::string)argv[3]).length() == 0)
+	{
+		std::cout << "Error: argument can't be empty" << std::endl;
+		exit(4);
+	}
 }
-	if(!infile)
-	{
-		std::cout << "Error: " << argv[1] << " not found" << std::endl;
-		exit(2);
-	}
-	buffer = argv[1];
-	buffer = buffer.append(".replace");
-	std::ofstream	outfile(buffer);
-	if (!outfile)
-	{
-		std::cout << "Error: unable to create " << buffer << std::endl;
-		exit(3);
-	}
-	while (std::getline(infile, buffer))
-	{
-		found = 0;
-		aux_index = 0;
-		std::cout << buffer << std::endl;
-		while (true)
-		{
-			found = buffer.find((std::string)argv[2], aux_index);
-			//std::cout << "text founded in psition " << found << std::endl;
-			if (found == std::string::npos)
-				break;
-			aux.append(buffer, aux_index, found - aux_index);
-			aux.append(std::string(argv[3]));
-			aux_index++;
-		}
-		//outfile.append(buffer, aux_index);
-		//outfile << aux << std::endl;
-	}
-	outfile.close();
+
+void	infile_error(char **argv)
+{
+	std::cout << "Error: " << argv[1] << " not found" << std::endl;
+	exit(2);
+}
+
+void	outfile_error(std::string buffer)
+{
+	std::cout << "Error: unable to create " << buffer << std::endl;
+	exit(3);	
 }
