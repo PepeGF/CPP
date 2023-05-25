361e2d271e005f78da756c188670e437198c8491
PepeGF <gran.pepe@gmail.com>
2023-05-25 19:24:02 +0200

    ex06 fixed for linux and mac

--- CPP01/ex06/Harl.cpp
+++ CPP01/ex06/Harl.cpp
@@ -61,10 +61,13 @@ void Harl::complain(std::string level)
 	{
 		case 0:
 			Harl::debug();
+			__attribute__((fallthrough));
 		case 1:
 			Harl::info();
+			__attribute__((fallthrough));
 		case 2:
 			Harl::warning();
+			__attribute__((fallthrough));
 		case 3:
 			Harl::error();
 			break;
