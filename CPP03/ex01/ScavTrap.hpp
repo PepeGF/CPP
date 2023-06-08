78bde360df4394f7a68b0eb43fe74f24785f7b11
PepeGF <gran.pepe@gmail.com>
2023-06-08 22:12:41 +0200

    starting cpp3/01

---
+++ CPP03/ex01/ScavTrap.hpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "ClapTrap.hpp"
+
+class ScavTrap : public ClapTrap
+{
+	public:
+		ScavTrap();
+		ScavTrap(std::string name);
+		ScavTrap(const ScavTrap &other);
+		~ScavTrap();
+
+		ScavTrap &operator=(const ScavTrap &rhs);
+
+		void	attack(const std::string &target);
+
+};
---
+++ CPP03/ex01/main.cpp
@@ -0,0 +1,42 @@
+#include "ClapTrap.hpp"
+
+int main()
+{
+	ClapTrap voldemort;
+	ClapTrap onesiforo("Onesiforo");
+	ClapTrap *lupinicio = new ClapTrap("Lupinicio");
+	ClapTrap *filogonio = new ClapTrap(*lupinicio);
+
+	std::cout << std::endl;
+
+	for (int i = 0; i <= 10; i++)
+	{
+		onesiforo.attack("lupinicio");
+		lupinicio->takeDamage(onesiforo.getAttackDamage());
+		if (i >= 3)
+			onesiforo.setAttackDamage(i+1);
+	}
+
+	std::cout << std::endl;
+
+	std::cout << voldemort.getName() << ": ";
+	voldemort.beRepaired(1000000);
+	std::cout 	
+		<< voldemort.getName() << " has " 
+		<< voldemort.getEnergyPoints() 
+		<< " energy points and " << voldemort.getHitpoints() 
+		<< " hitpoints" << std::endl;
+	lupinicio->beRepaired(100);
+	onesiforo.beRepaired(100);
+	std::cout << std::endl;
+
+	filogonio->attack("Onesiforo");
+
+	std::cout << std::endl;
+
+	delete(lupinicio);
+	delete(filogonio);
+
+	// system("leaks a.out");
+	return (0);
+}
