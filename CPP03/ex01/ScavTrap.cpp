44ab662acb71f20b4d2eb226069f049b919de986
José García <gran.pepe@gmail.com>
2023-10-31 19:00:52 +0100

    ex01 all review

--- CPP03/ex01/ScavTrap.cpp
+++ CPP03/ex01/ScavTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: josgarci <josgarci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/06 18:17:32 by josgarci          #+#    #+#             */
-/*   Updated: 2023/10/31 18:51:43 by josgarci         ###   ########.fr       */
+/*   Updated: 2023/10/31 18:59:56 by josgarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,16 +55,16 @@ void ScavTrap::attack(const std::string &target)
 {
 	if (this->_hit_points <= 0)
 	{
-		std::cout << this->_name << " unable to attack, it's dead!!!" << std::endl;
+		std::cout << this->getName() << " unable to attack, it's dead!!!" << std::endl;
 		return;
 	}
 	if (this->_energy_points <= 0)
 	{
-		std::cout << this->_name << " unable to attack, no energy points available" << std::endl;
+		std::cout << this->getName() << " unable to attack, no energy points available" << std::endl;
 		return;
 	}
-	std::cout << "ScavTrap " << this->_name << " attacks " 
-		<< target << ", causing " << this->_attack_damage 
+	std::cout << "ScavTrap " << this->getName() << " attacks " 
+		<< target << ", causing " << this->getAttackDamage() 
 		<< " points of damage!" << std::endl;
 	this->_energy_points -= 1;
 }
@@ -73,8 +73,8 @@ void ScavTrap::guardGate()
 {
 	if (this->_hit_points <= 0)
 	{
-		std::cout << this->_name << " unable to guard gate, it's dead!!!" << std::endl;
+		std::cout << this->getName() << " unable to guard gate, it's dead!!!" << std::endl;
 		return;
 	}
-	std::cout << "ScavTrap " << this->_name << " is now in Gate keeper mode" << std::endl;
+	std::cout << "ScavTrap " << this->getName() << " is now in Gate keeper mode" << std::endl;
 }
