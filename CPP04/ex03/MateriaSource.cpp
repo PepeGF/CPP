0eee3328796600b3eeeaab4fe80d6386c995b1fd
PepeGF <gran.pepe@gmail.com>
2023-12-08 21:18:49 +0100

    MateriaSource finished

--- CPP04/ex03/MateriaSource.cpp
+++ CPP04/ex03/MateriaSource.cpp
@@ -1,40 +1,50 @@
 #include "MateriaSource.hpp"
 #include "AMateria.hpp"
+#include "Cure.hpp"
+#include "Ice.hpp"
 
 MateriaSource::MateriaSource()
 {
 	for (int i = 0; i < 4; i++)
 	{
-		this->_materia[i] = NULL
+		this->_materia[i] = NULL;
 	}
 	std::cout << "MateriaSource default constructor called" << std::endl;
 }
 
-MateriaSource::MateriaSource(MateriaSource const & rhs) : IMateriaSource()
+MateriaSource::MateriaSource(MateriaSource const & copy) : IMateriaSource()
 {
-	if (this != &rhs)
+	for (int i = 0; i < 4; i++)
 	{
-		for (int i = 0; i < 4; i++)
+		if (copy._materia[i])
 		{
-			this->_materia[i] = rhs._materia[i]
+			if (copy._materia[i]->getType() == "cure")
+				this->_materia[i] = new Cure();
+			else if (copy._materia[i]->getType() == "ice")
+				this->_materia[i] = new Ice();
 		}
 	}
 
 	std::cout << "MateriaSource copy constructor called" << std::endl;
-	return (*this);
 }
 
 MateriaSource::~MateriaSource()
 {
+	for (int i = 0; i < 4; i++)
+	{
+		if (this->_materia[i])
+			delete this->_materia[i];
+	}
 	std::cout << "MateriaSource destructor called" << std::endl;
 
 }
 
 MateriaSource& MateriaSource::operator=(const MateriaSource& rhs)
 {
-	for (int i = 0; i < 4; i++)
+	if (this != &rhs)
 	{
-		this->_materia[i] = copy._materia[i]
+		for (int i = 0; i < 4; i++)
+			this->_materia[i] = rhs._materia[i];
 	}
 	std::cout << "MateriaSource assignation operator called" << std::endl;
 	return (*this);
@@ -42,13 +52,28 @@ MateriaSource& MateriaSource::operator=(const MateriaSource& rhs)
 
 void MateriaSource::learnMateria(AMateria* materia)
 {
-	(void)materia;
-	// implementar
+	for (int i = 0; i < 4; i++)
+	{
+		if (this->_materia[i] == NULL)
+		{
+			this->_materia[i] = materia;
+			return ;
+		}
+	}
 	return ;
 }
 
-AMateria* createMateria(const std::string & type)
+AMateria* MateriaSource::createMateria(const std::string & type)
 {
-	AMateria* materia = new AMateria(type);
-	return (materia);
+	AMateria* new_materia = NULL;
+
+	for (int i = 0; i < 4; i++)
+	{
+		if (this->_materia[i] != NULL && this->_materia[i]->getType() == type)
+		{
+			new_materia = this->_materia[i]->clone();
+			break;
+		}
+	}
+	return (new_materia);
 }
