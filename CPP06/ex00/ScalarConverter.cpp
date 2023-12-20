835962c502418e43e5921126425eeb4eca163921
PepeGF <gran.pepe@gmail.com>
2023-12-20 17:58:46 +0100

    CPP06 files created. Understanding subject

---
+++ CPP06/ex00/ScalarConverter.cpp
@@ -0,0 +1,19 @@
+# include "ScalarConverter.hpp"
+
+ScalarConverter::ScalarConverter()
+{}
+
+ScalarConverter::ScalarConverter()
+{}
+
+ScalarConverter::ScalarConverter()
+{}
+
+ScalarConverter::~ScalarConverter()
+{}
+
+void ScalarConverter::convert(std::string& literal)
+{
+	(void)literal;
+	return ;
+}
---
+++ CPP06/ex00/ScalarConverter.hpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+class ScalarConverter
+{
+private:
+	ScalarConverter();
+	ScalarConverter();
+	ScalarConverter();
+	~ScalarConverter();
+	
+public:
+	static void convert(std::string& literal);
+};
+
+
