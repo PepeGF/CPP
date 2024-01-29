0a38f74c47a30d18b0b91605e7c6ff9ffa31fa74
PepeGF <gran.pepe@gmail.com>
2024-01-29 19:46:23 +0100

    7x02 array implemented, main tests pending

---
+++ CPP07/ex02/Array.hpp
@@ -0,0 +1,85 @@
+# pragma once
+# include <iostream>
+
+template<typename T>
+class Array
+{
+private:
+	T 				*_ptr;
+	unsigned int	_size;
+public:
+	Array();
+	Array(unsigned int size);
+	Array(Array const &copy);
+	~Array();
+
+	Array &operator=(Array const &rhs);
+	T &operator[](unsigned int index);
+
+	unsigned int const &size() const;
+};
+
+template<typename T>
+Array<T>::Array()
+{
+	this->_ptr = NULL;
+	this->_size = 0;
+}
+
+template<typename T>
+Array<T>::Array(unsigned int size) : _size(size)
+{
+	this->_ptr = new T[size];
+	for (int i = 0; i < static_cast<int>(size); i++)
+		this->_ptr[i] = 0;		
+}
+
+template<typename T>
+Array<T>::Array(Array const &copy)
+{
+	if (copy._ptr)
+	{
+		this->_size = copy._size;
+		this->_ptr = new T[copy._size];
+		for (int i = 0; static_cast<int>(copy._size); i++)
+			this->_ptr[i] = copy._ptr[i];
+	}
+	else
+	{
+		this->_ptr = NULL;
+		this->_size = copy._size;
+	}
+}
+
+template<typename T>
+Array<T>::~Array()
+{
+	delete[] _ptr;
+}
+
+template<typename T>
+Array<T> &Array<T>::operator=(Array const &rhs)
+{
+	if (this != &rhs && this->_ptr && rhs._ptr)
+	{
+		for (int i = 0; i < static_cast<int>(this->_size); i++)
+			this->_ptr[i] = rhs._ptr[i];
+		this->_size = rhs._size;
+	}
+	return (*this);
+}
+
+template<typename T>
+T &Array<T>::operator[](unsigned int index)
+{
+	if (index >= this->_size || !this->_ptr)
+		throw (std::out_of_range("Index out of range"));
+	else
+		return (this->_ptr[index]);	
+}
+
+template<typename T>
+unsigned int const &Array<T>::size() const
+{
+	return (this->_size);
+}
---
+++ CPP07/ex02/main.cpp
@@ -0,0 +1,20 @@
+# include "Array.hpp"
+
+
+
+int main( void )
+{
+	Array<int> array;
+	std::cout << "Dirección array por defecto: " << &array << std::endl;
+
+	Array<int> otro(42);
+	std::cout << "Array vacío: \nLongitud array: " << otro.size() 
+				<< "\nContenido del Array:\n";
+	for (int i = 0; i < 42 - 1; i++)
+	{
+		std::cout << otro[i] << ", ";
+	}
+	std::cout << otro[41] << std::endl;
+
+	return 0;
+}
