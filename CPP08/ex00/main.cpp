ac0623b2ae1f227a91e44ebdbf5558b9a913a263
PepeGF <gran.pepe@gmail.com>
2024-02-01 20:05:49 +0100

    8x00 vector investigation

--- CPP08/ex00/main.cpp
+++ CPP08/ex00/main.cpp
@@ -9,7 +9,7 @@ int main()
 
 	// v.push_back(42);
 
-	for (int i = 0; i < 2; i++)
+	for (int i = 0; i < 10; i++)
 		v.push_back(i*i*10);
 	// std::vector<int>::iterator it = std::find(v.begin(), v.end(), 160);
 
@@ -18,6 +18,12 @@ int main()
 	for (size_t i = 0; i < v.size(); i++)
 		std::cout << &v[i] << std::endl;
 
+	std::cout << "---------" << std::endl;
+
+	for (std::vector<int>::const_iterator it = v.cbegin(); it != v.cend(); it++)
+	{
+		std::cout << "-> " << *it << std::endl;
+	}
 	std::cout << "---------" << std::endl;
 
 	for (int &x : v)
