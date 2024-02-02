cff25f876bce369344edbf62e41ad8b0b76bef2b
PepeGF <gran.pepe@gmail.com>
2024-02-02 09:38:57 +0100

    7x02 tests added

--- CPP07/ex02/main.cpp
+++ CPP07/ex02/main.cpp
@@ -13,6 +13,31 @@ int main( void )
 		std::cout << otro[i] << ", ";
 	}
 	std::cout << otro[41] << std::endl;
+	std::cout << "Asignar valor fuera de rango:\n\t";
+	try
+	{
+		otro[500] = 1903;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+	std::cout << "Asignar valor índice negativo:\n\t";
+	try
+	{
+		otro[-500] = 1903;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+	std::cout << "Asignar valor válido:\n";
+	for (int i = 10; i < 20; i++)
+		otro[i] = -2 * i * i;
+	for (size_t i = 0; i < otro.size() - 1; i++)
+		std::cout << otro[i] << ", ";
+	std::cout << otro[41] << std::endl;
+	
 
 	return 0;
 }
