bfe37e101ffc42f38d3215fd1224236364f9ea99
PepeGF <gran.pepe@gmail.com>
2024-02-02 19:18:43 +0100

    8x00 completado

--- CPP08/ex00/easyfind.hpp
+++ CPP08/ex00/easyfind.hpp
@@ -1,9 +1,29 @@
 # include <algorithm>
 # include <vector>
+# include <iostream>
 
-template<typename Container, int num>
-Container& easyfind(Container& wololo, int num)
+template<typename Container>
+typename Container::iterator easyfind(Container& wololo, int num)
 {
-	T aux = std::find(wololo.begin(), wololo.end(), num);
-	return aux;
+	typename Container::iterator it = std::find(wololo.begin(), wololo.end(), num);
+	if (it == wololo.end())
+	{
+		std::cout << "No se ha encontrado el valor " << num << " en el contenedor" << std::endl;
+		throw std::runtime_error("Value not found");
+	}
+	return it;
 }
+
+// si el subject de correccción no dice lo contrario esta implementación no es la que quiero:
+
+/* template<typename Container>
+int easyfind(Container& wololo, int num)
+{
+	typename Container::iterator it = std::find(wololo.begin(), wololo.end(), num);
+	if (it == wololo.end())
+	{
+		std::cout << "No se ha encontrado el valor " << num << " en el contenedor" << std::endl;
+		throw std::runtime_error("Value not found");
+	}
+	return *it;
+} */
--- CPP08/ex00/main.cpp
+++ CPP08/ex00/main.cpp
@@ -1,22 +1,36 @@
-// #include "easyfind.hpp"
-# include <iostream>
-# include <vector>
-# include <algorithm>
-# include "Wololo.hpp"
+#include "easyfind.hpp"
 
 int main()
 {
-
-
-
-
 	std::vector<int> v;
 
-	// v.push_back(42);
-
 	for (int i = 0; i < 10; i++)
+	{
 		v.push_back(i*i*10);
-	// std::vector<int>::iterator it = std::find(v.begin(), v.end(), 160);
+		std::cout << v[i] << " ";
+	}
+	std::cout << "\n---------------------------------------------" << std::endl;
+	std::cout << easyfind(v, 0)[0] << std::endl;
+	std::cout << easyfind(v, 360)[0] << std::endl;
+	try
+	{
+		std::cout << easyfind(v, 360000)[0] << std::endl;
+	}
+	catch(const std::runtime_error& e){}
+
+	/* std::cout << easyfind(v, 0) << std::endl;
+	std::cout << easyfind(v, 360) << std::endl;
+	try
+	{
+		std::cout << easyfind(v, 360000) << std::endl;
+	}
+	catch(const std::runtime_error& e){} */
+
+
+	return 0;
+}
+
+/* 	std::vector<int>::iterator it = std::find(v.begin(), v.end(), 160);
 
 	std::cout << "---------" << std::endl;
 
@@ -36,26 +50,9 @@ int main()
 		std::cout << x << std::endl;
 	}
 	std::cout << "---------" << std::endl;
-	// std::cout << *it << std::endl;
-	// std::cout << &it << std::endl;
-	// std::cout << &(*it) << std::endl;
+	std::cout << *it << std::endl;
+	std::cout << &it << std::endl;
+	std::cout << &(*it) << std::endl;
 	std::cout << "---------" << std::endl;
 	std::cout << v.size() << std::endl;
-	return 0;
-/* 
-	Wololo uno(2, "hola");
-	Wololo otro(uno);
-
-	uno.print();
-	std::cout << "-------------------" << std::endl;
-	otro.print();
-	std::cout << std::endl << "-x-x-x-x-x-x-x-x-x-" << std::endl << std::endl;
-
-	uno.patata[2] = 424242;
-	uno.print();
-	std::cout << "-------------------" << std::endl;
-	otro.print();
-
-	return 0; */
-
-}
+ */
