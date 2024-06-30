251d65921db8a9fe5b03c45d88c8f529baaf26eb
PepeGF <gran.pepe@gmail.com>
2024-06-30 20:09:10 +0200

    empezando 9x01

---
+++ CPP09/ex01/main.cpp
@@ -0,0 +1,17 @@
+# include "RPN.hpp"
+
+int main(int argc, char *argv[])
+{
+    if (argc == 2)
+    {
+        std::stack<int> numbers;
+        std::stack<char> symbols;
+    }
+    else
+    {
+        std::cerr << "Error" << std::endl;
+    }
+    return 0;
+}
+
+
