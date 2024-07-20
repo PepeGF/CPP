696cd4fe2427b444c9144af34ee995fd761c70a5
PepeGF <gran.pepe@gmail.com>
2024-07-21 00:44:16 +0200

    CPP07 improved

--- CPP07/ex01/iter.hpp
+++ CPP07/ex01/iter.hpp
@@ -3,9 +3,9 @@
 # include <limits.h>
 
 template<typename T>
-void iter(T* address, size_t len, void (*f)(T&))
+void iter(T* address, int len, void (*f)(T&))
 {
-    for (int i = 0; i < static_cast<int>(len); i++)
+    for (int i = 0; i < len; i++)
     {
         f(address[i]);
     }
@@ -15,4 +15,17 @@ template<typename T>
 void print(T& something)
 {
     std::cout << something << std::endl;
+    return;
 }
+
+/* class Awesome
+{
+    public:
+        Awesome(void): _n(42){return;}
+        int get(void) const {return this->_n;}
+    private:
+        int _n;
+};
+
+std::ostream& operator<<(std::ostream &o, Awesome const &rhs)
+{o << rhs.get(); return o;} */
--- CPP07/ex01/main.cpp
+++ CPP07/ex01/main.cpp
@@ -5,12 +5,17 @@
 int main( void ) 
 {
 	int array[] = {42, -42, 0, INT_MAX, INT_MIN};
-	size_t len = sizeof(array) / sizeof(int);
+	int len = sizeof(array) / sizeof(int);
 	iter(array, len, print);
 
 	std::string array_char[] = {"Hola", "mundo", "Forza Atleti"};
-	len = sizeof(array_char) / sizeof(int);
+	len = sizeof(array_char) / sizeof(std::string);
 	iter(array_char, len, print);
-
 	return 0;
 }
+
+	/* int tab[] = {0, 1, 2, 3, 4};
+	Awesome tab2[5];
+	len = sizeof(tab2)/sizeof(Awesome);
+	iter(tab, 5, print);
+	iter(tab2, len, print); */
