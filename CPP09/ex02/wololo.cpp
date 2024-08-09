5b695f073b895d033189e7442a2e1a1238918aea
PepeGF <gran.pepe@gmail.com>
2024-08-09 15:06:56 +0200

    merge raro

--- CPP09/ex02/wololo.cpp
+++ CPP09/ex02/wololo.cpp
@@ -51,3 +51,42 @@ int main(int argc, char* argv[]) {
 118, 119, 176, 189, 300, 392, 395, 400, 472, 474, 482, 485, 500, 541, 577, 658, 755, 777, 789, 900, 931
 
  */
+
+VecPairInt sort_with_insertion(VecPairInt &bigger_sort, VecPairInt &bigger_replica, VecPairInt &smaller, VecPairInt &smaller_replica, PairInt &last)
+{
+	(void)bigger_sort, (void)bigger_replica; (void)smaller; (void)smaller_replica; (void)last;
+
+	std::vector<int> jacobsthal_serie;
+	std::vector<int> index_aux;
+	VectPairIntIter it = bigger_replica.begin();
+	int insert_idx;
+	int bigger_replica_len;
+
+	bigger_replica_len = static_cast<int>(bigger_replica.size());
+	jacobsthal_serie = create_serie(smaller.size() - 1);
+	for (int i = 0; i < bigger_replica_len; i++)
+		index_aux.push_back(i);
+	for (int i = 0; i < static_cast<int>(smaller_replica.size()); i++)
+	{
+		insert_idx = binary_search(bigger_replica, 
+									  smaller_replica[jacobsthal_serie[i]], 
+									  0, 
+									  jacobsthal_serie[i] == static_cast<int>(bigger_replica.size()) 
+										  ? static_cast<int>(bigger_sort.size()) - 1 
+										  : index_aux[jacobsthal_serie[i]]);		
+		bigger_replica.insert(it + insert_idx, smaller_replica[jacobsthal_serie[i]]);
+		for (size_t j = 0; j < index_aux.size(); j++)
+		{
+			if (insert_idx <= index_aux[j])
+				index_aux[j]++;
+		}
+		// if (last.second != -1)
+		// 	insert_idx = binary_insertion(bigger_sort, 
+		// 									last, 
+		// 									0, 
+		// 									static_cast<int>(bigger_sort.size()) - 1);
+	}
+	// std::cout << "\nBIGGER_SORT:\n";
+	// print_vector(bigger_sort);
+	return bigger_replica;
+}
